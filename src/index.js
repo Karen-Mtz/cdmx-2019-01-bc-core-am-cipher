bf7c1aa92d234cfc7441f22ddb51ecbddca3b4b6
Karen-Mtz <dkareen.mtz@gmail.com>
2019-01-17 19:44:39 -0600

    prueba funcion

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-window.cipher.encode()
+window.cipher.encode(offset, palabraParaEncriptar)
 let palabraIngresada = document.getElementById("palabraIngresada").value;
 let palabraParaEncriptar = palabraIngresada.toUpperCase()
 let num = document.getElementById("offset").value;
 let offset = parseInt(num)
 let palabraEncriptada = []
+document.getElementById('palabraCifrada').innerHTML= 'Tu nueva contraseña es: ' + contrasenaNueva; 
