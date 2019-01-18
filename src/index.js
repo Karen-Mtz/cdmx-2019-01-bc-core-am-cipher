0fbb968e82cd1d8eae7fe0af18ded3688bf5f1e2
Karen-Mtz <dkareen.mtz@gmail.com>
2019-01-17 20:07:35 -0600

    intento test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-window.cipher.encode(offset, palabraParaEncriptar)
-let palabraIngresada = document.getElementById("palabraIngresada").value;
-let palabraParaEncriptar = palabraIngresada.toUpperCase()
-let num = document.getElementById("offset").value;
-let offset = parseInt(num)
-let palabraEncriptada = []
-document.getElementById('palabraCifrada').innerHTML= 'Tu nueva contraseña es: ' + contrasenaNueva; 
+const palabra = document.getElementById("palabraIngresada")
+const number = document.getElementById("num") 
+
+cifrar.addEventListener('click', () => {
+    let palabraIngresada = palabra.value;
+    let palabraParaEncriptar = palabraIngresada.toUpperCase();
+    let numeroIngresado = number.value;  
+    let offset = parseInt(numeroIngresado)  
+    let palabraEncriptada = []
+    window.cipher.encode(offset, palabraParaEncriptar)
+    console.log(contrasenaNueva)
+    document.getElementById('palabraCifrada').innerHTML= 'Tu nueva contraseña es: ' + contrasenaNueva; 
+})
+
+
