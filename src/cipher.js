c7cc13543ee310a6b6f692f87ed64623a47ac9ec
Karen-Mtz <dkareen.mtz@gmail.com>
2019-01-17 19:26:37 -0600

    subiendo prueba1

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,13 @@
 window.cipher = {
-  // ... 
+const: encode = () => {
+for (let i = 0; i <= palabraParaEncriptar.length; i++) { // bucle que recorre cada caracter de la palabra 
+let posicionEnCipher = (palabraParaEncriptar.charCodeAt(i) - 65 + offset) % 26 + 65 // sacar el codigo ascii de texto con charcodeat(i), aplicar formula para hacer el cifrado y guardar el resultado en una variable porque es la nueva posicion de cada letra
+console.log(posicionEnCipher)
+let nuevaLetra = (String.fromCharCode(posicionEnCipher)) //string.fromcharcode convierte el código ascii en letra
+palabraEncriptada.push(nuevaLetra) //agrega las letras nuevas al array
+let contrasenaNueva = palabraEncriptada.join('');
+console.log(contrasenaNueva)
+document.getElementById('palabraCifrada').innerHTML= 'Tu nueva contraseña es: ' + contrasenaNueva; 
+}
+}
 };
---
+++ src/index.js
@@ -0,0 +1,6 @@
+window.cipher.encode()
+let palabraIngresada = document.getElementById("palabraIngresada").value;
+let palabraParaEncriptar = palabraIngresada.toUpperCase()
+let num = document.getElementById("offset").value;
+let offset = parseInt(num)
+let palabraEncriptada = []
